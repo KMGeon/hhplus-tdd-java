30e41c394c3c7c3933dc99206c6b41e35541b7a2
mugeon <pos04167@kakao.com>
2025-03-28 00:56:36 +0900

    log : thread logger

--- src/main/java/io/hhplus/tdd/controller/dto/ThreadLogger.java
+++ src/main/java/io/hhplus/tdd/controller/dto/ThreadLogger.java
@@ -1,18 +1,14 @@
 package io.hhplus.tdd.controller.dto;
 
-import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
-@Slf4j
 public abstract class ThreadLogger {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
 
     public static void log(Object obj) {
         String time = LocalTime.now().format(formatter);
-        String threadName = Thread.currentThread().getName();
-        String message = String.format("%s [%s] %s", time, threadName, obj);
-        log.debug(message);
+        System.out.printf("%s [%9s] %s\n", time, Thread.currentThread().getName(), obj);
     }
 }
